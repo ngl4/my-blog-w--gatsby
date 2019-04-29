496e956cc082227e552bf7d4a2d5b034df0a5a05
Binh Lam <cindybinh444@gmail.com>
2019-04-28 23:21:57 -0400

    add siteUrl again

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,7 @@ const postCssPlugins = require('./postcss-config.js');
 module.exports = {
   siteMetadata: {
     url: siteConfig.url,
-    siteUrl: 'https://stori3x.netlify.com',
+    siteUrl: `https://www.example.com`,
     title: siteConfig.title,
     subtitle: siteConfig.subtitle,
     copyright: siteConfig.copyright,
@@ -139,7 +139,7 @@ module.exports = {
           {
             site {
               siteMetadata {
-                url
+                siteUrl
               }
             }
             allSitePage(
