4a651a67071fb4516945ac0363f59c809aa2f95f
Binh Lam <cindybinh444@gmail.com>
2019-05-15 16:56:20 -0400

    add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,7 +30,14 @@ module.exports = {
           }
         ]
       }
-    },  
+    }, 
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // replace "UA-XXXXXXXXX-X" with your own Tracking ID
+        trackingId: "UA-140307899-1",
+      },
+    }, 
     {
       resolve: 'gatsby-source-filesystem',
       options: {
